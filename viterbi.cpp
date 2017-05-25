0f146bacd609974f857f2bd73488b52b73124dfd
vladimirbelinski <vladimir_belinski@hotmail.com>
2017-05-25 19:05:23 -0300

    Fixed/finished decoding function.

--- viterbi.cpp
+++ viterbi.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-    
+
 using namespace std;
 
 void init_table(map<string, map<char,string> > &table){
@@ -10,7 +10,7 @@ void init_table(map<string, map<char,string> > &table){
 }
 
 struct Context{
-  string initial_state; 
+  string initial_state;
   char input;
   string emitted_output, received, final_state;
   int error;
@@ -25,13 +25,13 @@ string encode(string bit_sequence){
   bit_sequence += "00";
   string state = "00";
   for(auto& bit : bit_sequence){
-    encoded += emitted[state][bit];     
+    encoded += emitted[state][bit];
     state = next_state[state][bit];
-  }  
+  }
   return encoded;
 }
 
-int diff(string str1, string str2){  
+int diff(string str1, string str2){
   return (str1[0] != str2[0]) + (str1[1] != str2[1]);
 }
 
@@ -47,33 +47,35 @@ int lowest_error(vector<int> &vi){
 }
 
 string decode(string bit_sequence){
-  string decoded = "";    
+  string decoded = "";
   string prefix = bit_sequence.substr(0,2);
-  
+
   /*step 1.*/
-  int err00 = diff(prefix,"00"), err10 = diff(prefix,"11");  
+  int err00 = diff(prefix,"00"), err10 = diff(prefix,"11");
   decoded += err00 < err10 ? '0': '1';
-  
+
   /*step 2.*/
-  prefix = bit_sequence.substr(2,2);    
-  vector<int> diffs({diff(prefix,"00")+err00,diff(prefix,"11")+err00,diff(prefix,"10")+err10,diff(prefix,"01")+err10});      
-  int lowest_error_index = lowest_error(diffs);   
-  decoded += (lowest_error_index & 1) + '0';  
-  //cout << bit_sequence << endl;
+  prefix = bit_sequence.substr(2,2);
+  vector<int> diffs({diff(prefix,"00")+err00,diff(prefix,"11")+err00,diff(prefix,"10")+err10,diff(prefix,"01")+err10});
+  int lowest_error_index = lowest_error(diffs);
+  decoded += (lowest_error_index & 1) + '0';
+  // cout << bit_sequence << endl;
+
   /*General step.*/
-  for(int i = 4; i < (int)bit_sequence.size(); i += 2){    
-    prefix = bit_sequence.substr(i,2);      
-    vector<int> cur_diffs(4);        
-    cur_diffs[0] = min(diff(prefix,"00")+diffs[0],diff(prefix,"11")+diffs[3]);
-    cur_diffs[1] = min(diff(prefix,"01")+diffs[2],diff(prefix,"10")+diffs[1]);    
-    cur_diffs[2] = min(diff(prefix,"11")+diffs[0],diff(prefix,"00")+diffs[3]);
-    cur_diffs[3] = min(diff(prefix,"10")+diffs[2],diff(prefix,"01")+diffs[1]);    
-    int lowest_error_index = lowest_error(cur_diffs); 
-    cout << lowest_error_index << endl;      
-    decoded += (lowest_error_index & 1) + '0';        
-    for(int k = 0; k < 4; k++) diffs[k] = cur_diffs[k];      
+  /*[index]=state: [0]=00, [1]=10, [2]=01, [3]=11*/
+  for(int i = 4; i < (int)bit_sequence.size(); i += 2){
+    prefix = bit_sequence.substr(i,2);
+    vector<int> cur_diffs(4);
+    cur_diffs[0] = min(diff(prefix,"00")+diffs[0],diff(prefix,"11")+diffs[2]);
+    cur_diffs[1] = min(diff(prefix,"11")+diffs[0],diff(prefix,"00")+diffs[2]);
+    cur_diffs[2] = min(diff(prefix,"10")+diffs[1],diff(prefix,"01")+diffs[3]);
+    cur_diffs[3] = min(diff(prefix,"01")+diffs[1],diff(prefix,"10")+diffs[3]);
+    int lowest_error_index = lowest_error(cur_diffs);
+    // cout << lowest_error_index << endl;
+    decoded += (lowest_error_index & 1) + '0';
+    for(int k = 0; k < 4; k++) diffs[k] = cur_diffs[k];
   }
-  cout << decoded << endl;
+  // cout << decoded << endl;
   return decoded.substr(0,decoded.size()-2);
 }
 
@@ -81,11 +83,11 @@ int main(void){
   int err_percentage;
   string bit_sequence, encoded, decoded;
   cin >> err_percentage;
-  cin >> bit_sequence; 
-   
+  cin >> bit_sequence;
+
   init_table(emitted);
   init_table(next_state);
-  
+
   emitted["00"]['0'] = "00";
   emitted["00"]['1'] = "11";
   emitted["01"]['0'] = "11";
@@ -94,7 +96,7 @@ int main(void){
   emitted["10"]['1'] = "01";
   emitted["11"]['0'] = "01";
   emitted["11"]['1'] = "10";
-  
+
   next_state["00"]['0'] = "00";
   next_state["00"]['1'] = "10";
   next_state["01"]['0'] = "00";
@@ -105,10 +107,10 @@ int main(void){
   next_state["11"]['1'] = "11";
 
   encoded = encode(bit_sequence);
-  
+
   decoded = decode(encoded);
-  cout << bit_sequence << endl;
+  cout << "Initial sequence: " << bit_sequence << endl;
   //cout << encoded << endl;
-  cout << decoded << endl;
+  cout << "Decoded sequence: " << decoded << endl;
   return 0;
 }
